b2df99cd7aad4c782e791143680b65355850d23b
gab-in <138691076+gab-in@users.noreply.github.com>
2023-12-13 20:56:54 -0300

    Delete Trabalho Bimestral Desevolvimento Web directory

--- Trabalho Bimestral Desevolvimento Web/js/script.js
+++
@@ -1,9 +0,0 @@
-function adicionar()
-{
- var tarefa = document.getElementById('tar1').value;
- document.getElementById('tabletarefa').innerHTML +='<tr id="tarefapen">'+'<td>'+'</td>'+'<td>'+tarefa+'</td>'+'<td>'+'<button onclick=remover(event.target)>Concluir</button>'+'</td>'+'</tr>';
-}
-
- function remover(elementoClicado) {
-    elementoClicado.closest("tr").remove();
-  }
